b5d2bce896aa81c5c7ffdc6628a7ca853d26d6a0
Lê Nguyễn Nhật Quang <quanglnn2-la-01@SGN-LA-QUANGLNN2.local>
2026-04-09 23:15:11 +0700

    testing

--- scripts/insert_demo.sql
+++ scripts/insert_demo.sql
@@ -2,7 +2,7 @@ USE inventory_demo;
 GO
 
 EXEC dbo.sp_insert_demo_transaction
-    @warehouse_id = 'WH03',
+    @warehouse_id = 'WH06',
     @sku_id = 'SKU_001',
     @event_type = 'SHIPMENT',
     @qty_change = -50,
